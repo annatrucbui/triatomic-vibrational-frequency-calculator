142471a695ab55dc48e2b88602d797de63d85e82
annatrucbui <48100681+annatrucbui@users.noreply.github.com>
2020-12-25 20:51:29 +0000

    Changed test for all file extraction

--- vib-freq-calculator-test.py
+++ vib-freq-calculator-test.py
@@ -6,24 +6,25 @@ Created on Sat Oct 24 17:16:15 2020
 """
 
 import unittest
-import numpy as np
-import VibFreqCalculator as vfc
+
+import vfc_functions as vfc
 
 class DataExtraction(unittest.TestCase):
+    
     def testGeometry(self):
+        pathname ="./H2Ooutfiles/"
         filename = "H2O.r0.70theta75.0.out"
         expected = (0.7, 75.0)
-        self.assertTupleEqual(vfc.gemeotry_energy(filename)[0:2], expected)
+        self.assertTupleEqual(vfc.get_gemeotry_energy(pathname,filename)[0:2], expected)
     def testEnergy(self):
+        pathname ="./H2Ooutfiles/"
         filename = "H2O.r0.70theta75.0.out"
         expected = -75.7253171225
-        self.assertEqual(vfc.gemeotry_energy(filename)[2], expected)
+        self.assertEqual(vfc.get_gemeotry_energy(pathname,filename)[2], expected)
     def testAllFilesExtracted(self):
+        pathname ="./H2Ooutfiles/"
         total = 2275
-        self.assertEqual(len(vfc.bondlengths), total)
-        self.assertEqual(len(vfc.bondangles), total)
-        self.assertEqual(len(vfc.energies), total)
-        
+        self.assertEqual(len(vfc.dictionary_PES(pathname)), total)
         
 
 if __name__ == "__main__":
